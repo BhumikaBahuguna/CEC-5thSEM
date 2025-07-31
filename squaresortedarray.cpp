a82b50e0aaa6d8447ced96fbcd0a6a9e0239be1c
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-07-31 23:35:10 +0530

    squaresortedarray.cpp

---
+++ squaresortedarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res;
+        int n=nums.size();
+        int l=0,r=n-1;
+        while(l<=r){
+            if(nums[l]*nums[l]> nums[r]*nums[r]){
+                res.push_back(nums[l]*nums[l]);
+                l++;
+            }else{
+                res.push_back(nums[r]*nums[r]);
+                r--;
+            }
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
